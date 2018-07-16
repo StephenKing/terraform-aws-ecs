20303e47e4a0a7fbbfa9100d9289f03b6a6da521
Steffen Gebert <steffen.gebert@typo3.org>
2018-07-16 06:52:07 +0200

    Use vpc module

--- ecs.tf
+++ ecs.tf
@@ -7,24 +7,44 @@ variable "ssh_key_name" {
 }
 
 variable "cluster_size" {
-  default = 2
+  default = 1
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.10.0.0/16"
-}
+//resource "aws_vpc" "main" {
+//  cidr_block = "10.10.0.0/16"
+//}
 
 data "aws_region" "current" {}
 
-resource "aws_subnet" "private" {
-  count      = 3
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, "${count.index}")}"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  tags {
-    Name = "private ${var.stage}"
+  name = "tf-test"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
+
+//resource "aws_subnet" "private" {
+//  count      = 3
+//  vpc_id     = "${module.vpc.vpc_id}"
+//  cidr_block = "${cidrsubnet(module.vpc.cidr_block, 8, "${count.index}")}"
+//
+//  tags {
+//    Name = "private ${var.stage}"
+//  }
+//}
 
 resource "aws_ecs_cluster" "default" {
   name = "${var.stage}"
@@ -32,7 +52,7 @@ resource "aws_ecs_cluster" "default" {
 
 resource "aws_security_group" "ecs_instance" {
   name = "ecs-instance-${var.stage}"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${module.vpc.vpc_id}"
 
   # TODO secure
   egress {
@@ -61,7 +81,7 @@ resource "aws_spot_fleet_request" "ecs" {
     iam_instance_profile_arn = "${aws_iam_instance_profile.ecs_instance.arn}"
 
     # TODO can be only one subnet???
-    subnet_id              = "${aws_subnet.private.0.id}"
+    subnet_id              = "${module.vpc.private_subnets[0]}"
     vpc_security_group_ids = ["${aws_security_group.ecs_instance.id}"]
     user_data              = "${data.template_cloudinit_config.ecs_init.rendered}"
 
